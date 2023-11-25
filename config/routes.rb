e9e70909fcf02b2a6f7be0074043bf19835992b2
Gladston Pereira <gladston.bosco@gmail.com>
2023-11-25 08:52:51 -0700

    added devsie stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :athletes
   get "up" => "rails/health#show", as: :rails_health_check
   root "athletes#index"
+  #root "users#index"
 end
