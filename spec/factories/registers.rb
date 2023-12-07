6a89df4b0397e1de1595bd927a4cafd252a71c6c
Gladdy17 <gladston.bosco@gmail.com>
2023-12-07 16:46:15 -0700

    adding test

--- spec/factories/registers.rb
+++ spec/factories/registers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :register do
-    First_Name { "MyString" }
-    Last_Name { "MyString" }
+    First_Name { "Gladdy" }
+    Last_Name { "Pereira" }
     Date_of_Birth { "2023-12-05" }
   end
 end
--- spec/models/register_spec.rb
+++ spec/models/register_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Register, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Register, type: :model do 
+  described 'validations' do 
+    let (:Register) { Register.new }
+
+      it 'has a invalid name' do 
+        expect(Register).not_to be_valid
+      end
+ 
 end
